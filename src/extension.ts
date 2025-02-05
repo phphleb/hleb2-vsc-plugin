55e177a1a6a6d9c0e583968f5b1d66561447941f
Foma Tuturov <fomiash@yandex.ru>
2025-02-05 18:13:58 +0300

    add debug info provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 import * as vscode from 'vscode';
 import {registerRouteAddressProvider} from "./routeAddressProvider";
 import {registerRoutePrefixProvider} from "./routePrefixProvider";
+import {registerDebugInfoProvider} from "./registerDebugInfoProvider";
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('The extension for the HLEB2 framework is activated.');
 
 	registerRouteAddressProvider(context);
 	registerRoutePrefixProvider(context);
+	registerDebugInfoProvider(context);
 }
 
 export function deactivate() {}
---
+++ src/registerDebugInfoProvider.ts
@@ -0,0 +1,80 @@
+import * as vscode from 'vscode'
+
+// Регулярное выражение для поиска вызова функций debug.
+// Функции: var_dump(), dd(), print_r(), var_dump2(), dump().
+// Дополнительно захватывается первый аргумент.
+const debugFunctionRegex = /(?:var_dump|dd|print_r|var_dump2|dump)\(\s*((?:"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|[^,)\s]+))/g
+
+export function registerDebugInfoProvider(context: vscode.ExtensionContext) {
+
+    const decorationType = vscode.window.createTextEditorDecorationType({
+        textDecoration: 'underline solid yellow'
+    })
+
+    function updateDecorations() {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        const text = editor.document.getText();
+        const decorations: vscode.DecorationOptions[] = [];
+
+        let match: RegExpExecArray | null;
+        while ((match = debugFunctionRegex.exec(text)) !== null) {
+            const argText = match[1];
+            const argIndex = match[0].indexOf(argText);
+            const startOffset = match.index + argIndex;
+            const endOffset = startOffset + argText.length;
+            const startPos = editor.document.positionAt(startOffset);
+            const endPos = editor.document.positionAt(endOffset);
+            const range = new vscode.Range(startPos, endPos);
+            decorations.push({ range });
+        }
+
+        editor.setDecorations(decorationType, decorations);
+    }
+
+    if (vscode.window.activeTextEditor) {
+        updateDecorations()
+    }
+
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        if (editor) {
+            updateDecorations();
+        }
+    }, null, context.subscriptions)
+
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
+            updateDecorations();
+        }
+    }, null, context.subscriptions)
+
+    const hoverProvider = vscode.languages.registerHoverProvider('*', {
+        provideHover(document, position) {
+            const entireText = document.getText();
+            let match: RegExpExecArray | null;
+
+            while ((match = debugFunctionRegex.exec(entireText)) !== null) {
+                const argText = match[1];
+                const argIndex = match[0].indexOf(argText);
+                const startOffset = match.index + argIndex;
+                const endOffset = startOffset + argText.length;
+                const startPos = document.positionAt(startOffset);
+                const endPos = document.positionAt(endOffset);
+                const range = new vscode.Range(startPos, endPos);
+
+                if (range.contains(position)) {
+                    const markdown = new vscode.MarkdownString();
+                    markdown.appendMarkdown('HLEB2 Info: Output of debugging information.');
+                    markdown.isTrusted = true;
+                    return new vscode.Hover(markdown, range);
+                }
+            }
+            return undefined;
+        }
+    })
+
+    context.subscriptions.push(hoverProvider)
+}
