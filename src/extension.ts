170b4d968d7bc7fa53b49fc3d8941da022ee1ec4
Foma Tuturov <fomiash@yandex.ru>
2025-02-07 12:55:09 +0300

    add root path

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 import {registerRouteAddressProvider} from "./routeAddressProvider";
 import {registerRoutePrefixProvider} from "./routePrefixProvider";
 import {registerDebugInfoProvider} from "./registerDebugInfoProvider";
@@ -6,8 +8,14 @@ import {registerDebugInfoProvider} from "./registerDebugInfoProvider";
 export function activate(context: vscode.ExtensionContext) {
 	console.log('The extension for the HLEB2 framework is activated.');
 
-	registerRouteAddressProvider(context);
-	registerRoutePrefixProvider(context);
+	const workspaceRoots = vscode.workspace.workspaceFolders;
+	let root: string | null = workspaceRoots ? workspaceRoots[0].uri.fsPath : null;
+
+	if (root && fs.existsSync(path.join(root, 'app', 'Bootstrap', 'BaseContainer.php'))) {
+		registerRouteAddressProvider(context);
+		registerRoutePrefixProvider(context);
+	}
+
 	registerDebugInfoProvider(context);
 }
 
